419f46350295fbd4c1af84aaaf544baefe329d6c
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-08 10:47:12 -0300

    mudança na string da Pa

--- exercicios/ex61.py
+++ exercicios/ex61.py
@@ -4,11 +4,11 @@ razao = int(input('Digite a razão da PA:'))
 termo = 1
 termo_final = 11
 while termo < termo_final:#enquanto {termo} for menor que {termo_final} faça
-    print(termo, 'ª = ', primeiro_termo)#printa {termoª} e primeiro termo
+    print(termo, 'ª termo = ', primeiro_termo, end=' | ')#printa {termoª} e primeiro termo
     primeiro_termo += razao#soma {primeiro_termo} com a {razão}
     termo += 1#soma 1 em {termo}
     if termo == termo_final:#se {termo} igual ao termo final faça: vai dar oportunidade de acrescentar mais termos
-        termo_final += int(input('Estes foram os termos! Quer ver mais quantos?:'))
+        termo_final += int(input('\nEstes foram os termos! Quer ver mais quantos?:'))
 #lógica termo começa com 1
 #termo final com 11
 #while termo < termo_final vai rodar adicionando += 1 no termo, quando termo chegar a 10
