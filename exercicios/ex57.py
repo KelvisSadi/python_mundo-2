3955d52064d65bdec0f9926115b21de7a9d5e2f7
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-08 09:00:37 -0300

    mudança na condição do while

--- exercicios/ex57.py
+++ exercicios/ex57.py
@@ -1,6 +1,6 @@
 print('IBGE')
 nome = str(input('Digite seu nome:')).strip()
 sexo = str(input('Digite seu sexo [M/F]:')).lower().strip()
-while sexo != 'm' and sexo != 'f':#enquanto sexo for diferente de M e F faça
+while sexo not in 'mf':#enquanto sexo for diferente de M e F faça
     sexo = str(input('Digite novamente seu sexo! escolha entre --> [M/F]')).lower()
 print(f'{nome}, você concluiu nossa pesquisa!')
