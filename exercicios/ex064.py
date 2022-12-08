89c6b363e15c3458b537e981554aefdedfb7410a
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-08 16:01:13 -0300

    atualizando string do input do ex064.py

--- exercicios/ex064.py
+++ exercicios/ex064.py
@@ -1,9 +1,7 @@
 numeros = []
-num_entrada = 0
-soma = 0
-quantidade = 0
+num_entrada = soma = quantidade = 0 #n recebe s que recebe quant que recebe zero, ou seja todos recebem zero em 1 linha
 while num_entrada != 999:
-    num_entrada = int(input('Para sair do programa digite 999\nDigite um número para adicionar a soma:'))
+    num_entrada = int(input('Para sair do programa [digite 999]\nDigite um número para adicionar a soma:'))
     if num_entrada != 999:
         numeros.append(num_entrada)
         soma += num_entrada
