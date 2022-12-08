050f60a59d8de5adf8b8c8811d03f5b728e03daf
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-08 16:20:44 -0300

    diminuindo tamanhgo das variaveis e atualizando strings

--- exercicios/ex065.py
+++ exercicios/ex065.py
@@ -1,19 +1,16 @@
 print('Leitura de números')
 continuar = 's'
-quantidade = 0
-soma = 0
-media = 0
-while continuar == 's':
+quantidade = soma = media = 0#todos recebem zero em uma linha de código
+while continuar in 'Ss': #enquanto {continuar} tiver 'Ss' maiúsculo ou minúsculo faça o bloco
     num = int(input('Digite um número inteiro:'))
     quantidade += 1
-    if quantidade == 1:
+    if quantidade == 1:#se for a primeira vez rodando o looping {maior} e {menor} vao receber o {num}
+        maior = menor = num
+    if num > maior:#se {num} maior que {maior} faça
         maior = num
+    if num < menor:#se {num} menor que {menor} faça
         menor = num
-    if num > maior:
-        maior = num
-    if num < menor:
-        menor = num
-    soma += num
-    continuar = str(input('Quer continuar [S/N]:')).strip().lower()
-media = soma / quantidade
-print(f'A média de todos os valores digitados é:{media}, o maior valor é {maior} e o menor valor é {menor}')
+    soma += num#{soma} vai somar {num}
+    continuar = str(input('Quer continuar? [S/N]:')).strip()
+media = soma / quantidade#{media} vai dividir {soma} pela {quantidade}
+print(f'A média dos {quantidade} valores digitados é: {media}\nO maior valor é {maior}\nO menor valor é {menor}')
