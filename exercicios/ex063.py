8b64025be15a7ebd12250645a99d12ddedda8786
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-08 15:48:17 -0300

    atualizando sequência de fibonacci

--- exercicios/ex063.py
+++ exercicios/ex063.py
@@ -1,14 +1,13 @@
 print('Sequência de Fibonacci')
-n = int(input('Digite um número:'))
+n = int(input('Quantos termos quer mostrar:'))
 antes = 1
 atual = 0
 proximo = 0
 while n > 0:
-    proximo = antes + atual#Lógica: Primeiro calculamos o proximo termo com a soma dos dois termos de trás
-    print(proximo)#depois mostramos o termo que foi calculado
+    print(proximo, end=' -> ')  #depois mostramos o termo que foi calculado
+    proximo = antes + atual#Lógica: Calculamos o {proximo} termo com a soma dos dois termos de anteriores
     antes = atual#o termo mais antigo recebe o do meio
     atual = proximo#e o termo do meio recebe o que foi calculado, assim todos os termos foram atualizados e podemos calcular o próximo no looping
     n -= 1
-print('teste')
-
+print('FIM')
 #ex: [f1= 1, f2= 0, f3=?] -> [f1= 1, f2= 0, f3= f1 + f2]
