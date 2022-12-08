38a2ee4a6b9813757788ce4be73c86feb07c67e5
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-08 09:30:27 -0300

    implementação de chute alto, ou baixo mudança na variavel que usammos para verificar a condição while

--- exercicios/ex58.py
+++ exercicios/ex58.py
@@ -1,9 +1,15 @@
 from random import randint
 quantidade_tentativas = 0
 n_aleatorio = randint(1, 11)
-chute = 99
+acertou = False
 print('Tente acertar o número mágico entre 1 e 10')
-while chute != n_aleatorio:
+while not acertou:#enquanto acertou for falso
     chute = int(input('Seu chute:'))
     quantidade_tentativas += 1
+    if chute < n_aleatorio:#se chute menor que n_aleatório faça
+        print('Mais... Tente novamente!')
+    elif chute > n_aleatorio:#se chute maior que n_aleatório faça
+        print('Menos... Tente novamente!')
+    else:#se nao faça
+        acertou = True
 print(f'Você acertou o número mágico que era {n_aleatorio} em {quantidade_tentativas} tentativas')
