81a19df7ecf1008ae76c286d64635304b6eb5ec8
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-12 15:19:00 -0300

    atualização no ex69.py, add muitos whiles infinitos e break's

--- exercicios/ex69.py
+++ exercicios/ex69.py
@@ -1,23 +1,30 @@
 print('IBGE V2')
 qtd_maiores_18 = qtd_homens_cadastrados = qtd_mulheres_menos_20 = participantes = 0
+sexo = 'a'
+continuar = 's'
 contador = 1
-while True:
+while True:# enquanto infinito n1
+    print('-' * 40)
     idade = int(input(f'|{contador}ª Pessoa| Digite a idade:'))
-    sexo = str(input(f'|{contador}ª Pessoa| Digite o sexo [M/F]:')).strip().upper()
-    if sexo == 'M' or sexo == 'F':
-        if idade >= 18:
-            qtd_maiores_18 += 1
-        if sexo == 'M':
-            qtd_homens_cadastrados += 1
-        if sexo == 'F' and idade < 20:
-            qtd_mulheres_menos_20 += 1
-        participantes += 1
-        contador += 1
-    else:
-        print('Opção Invalida! Esolha o sexo com [M] ou [F] ')
+    while True:# enquanto infinito n2
+        print('-' * 40)
+        sexo = str(input(f'|{contador}ª Pessoa| Digite o sexo [M/F]:')).strip().upper()
+        if sexo == 'M' or sexo == 'F':
+            break#break n2
+    if idade >= 18:
+        qtd_maiores_18 += 1
+    if sexo == 'M':
+        qtd_homens_cadastrados += 1
+    if sexo == 'F' and idade < 20:
+        qtd_mulheres_menos_20 += 1
+    participantes += 1
+    contador += 1
     print('-'*40)
-    print('Digite [SAIR] para sair OU [QUALQUER TECLA] para continuar')
-    continuar = str(input('Sua Opção:')).strip().upper()
-    if continuar == 'SAIR':
-        break
+    while True:#enquanto infinito n3
+        print('Digite [S] para contiuar\nDigite [N] para sair')
+        continuar = str(input('Sua Opção:')).strip().upper()
+        if continuar == 'S' or continuar == 'N':
+            break#break n3
+    if continuar == 'N':
+        break#break n1
 print(f'Ao todo foram lidos {participantes} participantes, {qtd_homens_cadastrados} homens cadastrados e {qtd_mulheres_menos_20} mulheres com menos de 20 anos!')
