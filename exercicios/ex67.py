e54012dc5594f3b8d73b2ee02413ad0da351dab5
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-14 14:07:32 -0300

    atualizando o nome das variáveis e adicionando comentários

--- exercicios/ex67.py
+++ exercicios/ex67.py
@@ -1,12 +1,12 @@
 print('Tabuada V3')
 n = 0
-while True:
+while True:  # while infinito n1
     print('-'*40)
     n = int(input('Quer ver a tabuada de qual valor?'))
     print('-'*40)
     if n <= 0:
         print('Tabuada encerrada. Volte sempre!')
-        break
-    for t in range(1, 11):
-        res = n * t
-        print(f'{f"{n:<3} X {t:3} = {res:<3}":^40}')
+        break  # break n1
+    for tabuada in range(1, 11):
+        res = n * tabuada
+        print(f'{f"{n:<3} X {tabuada:3} = {res:<3}":^40}')
