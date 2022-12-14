51d3687f360cc7d0d1c181cc31719b38f05605dc
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-14 14:04:08 -0300

    atualizando o nome das variáveis

--- exercicios/ex66.py
+++ exercicios/ex66.py
@@ -1,12 +1,12 @@
 print('Leitor')
-n = c = s = 0
+num = cont = soma = 0
 digitados = []
 print('Para sair Digite [999]')
-while True:
-    n = int(input('Digite um número:'))
-    if n == 999:
-        break
-    c += 1
-    s += n
-    digitados.append(n)
-print(f'Você digitou {c} números, que são {digitados} e a soma deles é igual a: {s}')
+while True:  # while infinito n1
+    num = int(input('Digite um número:'))
+    if num == 999:
+        break  # break n1
+    cont += 1
+    soma += num
+    digitados.append(num)
+print(f'Você digitou {cont} valores, que são {digitados} e a soma deles é igual a: {soma}')
