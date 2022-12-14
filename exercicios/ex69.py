121fe9d551087e375e4bba27be7fcb06f0b86c43
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-14 15:16:54 -0300

    atualizando prints finais e condição de saída.

--- exercicios/ex69.py
+++ exercicios/ex69.py
@@ -3,14 +3,13 @@ qtd_maiores_18 = qtd_homens_cadastrados = qtd_mulheres_menos_20 = participantes 
 sexo = 'a'
 continuar = 's'
 contador = 1
-while True:# enquanto infinito n1
+while True:  # while infinito n1
     print('-' * 40)
     idade = int(input(f'|{contador}ª Pessoa| Digite a idade:'))
-    while True:# enquanto infinito n2
-        print('-' * 40)
-        sexo = str(input(f'|{contador}ª Pessoa| Digite o sexo [M/F]:')).strip().upper()
+    while True:  # while infinito n2
+        sexo = str(input(f'|{contador}ª Pessoa| Digite o sexo [M/F]:')).strip().upper()[0]
         if sexo == 'M' or sexo == 'F':
-            break#break n2
+            break  # break n2
     if idade >= 18:
         qtd_maiores_18 += 1
     if sexo == 'M':
@@ -20,15 +19,15 @@ while True:# enquanto infinito n1
     participantes += 1
     contador += 1
     print('-'*40)
-    while True:#enquanto infinito n3
-        print('Digite [S] para contiuar\nDigite [N] para sair')
-        continuar = str(input('Sua Opção:')).strip().upper()
-        if continuar == 'S' or continuar == 'N':
-            break#break n3
-    if continuar == 'N':
-        break#break n1
+    while True:  # while infinito n3
+        print('Digite [C] para continuar\nDigite [S] para sair')
+        continuar = str(input('Sua Opção:')).strip().upper()[0]
+        if continuar == 'C' or continuar == 'S':
+            break  # break n3
+    if continuar == 'S':
+        break  # break n1
 print(f'{" FIM DO PROGRAMA ":-^40}')
-print(f'{participantes} participantes!')
-print(f'{qtd_homens_cadastrados} homens cadastrados!')
-print(f'{qtd_mulheres_menos_20} mulheres com menos de 20 anos!')
+print(f'Total de {participantes} participantes!')
+print(f'Total de {qtd_homens_cadastrados} homens cadastrados!')
+print(f'Total de {qtd_mulheres_menos_20} mulheres com menos de 20 anos!')
 print(f'{qtd_maiores_18} pessoas com mais de 18 anos!')
