bc7f76f37e5427f6efdf2b8767f34d5046519519
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-15 09:10:54 -0300

    mudança na condição de continuar com while e simplificando a condição de menor valor.

--- exercicios/ex70.py
+++ exercicios/ex70.py
@@ -1,32 +1,29 @@
 print('Programa de Vendas')
-nome_produto = ''
-qtd_mais_de_mil = gasto_total = 0
+nome_produto = nome_mais_barato = ''
+qtd_mais_de_mil = gasto_total = mais_barato = 0
 contador = 1
 produtos = []
-while True:#while infinito n1
+while True:  # while infinito n1
     print('-'*40)
     nome_produto = str(input(f'Digite o nome do {contador}º prouto:'))
     print('-'*40)
     preço_produto = float(input(f'Digite o preço do {contador}º produto [{nome_produto}] R$:'))
-    produtos.append(nome_produto)#add na lista produtos
+    produtos.append(nome_produto)  # add na lista produtos
     gasto_total += preço_produto
-    if contador == 1:#se primeira vez
-        mais_barato = preço_produto
-    if preço_produto < mais_barato:#se mais barato
+    if contador == 1 or preço_produto < mais_barato:  # se primeira vez ou se aparecer um mais barato
         mais_barato = preço_produto
         nome_mais_barato = nome_produto
-    if preço_produto > 1000:#se mais de mil
+    if preço_produto > 1000:  # se mais de mil
         qtd_mais_de_mil += 1
     print('-'*40)
     print(f'O produto {nome_produto} foi adicionado.')
     print('-'*40)
     print('Digite [S] para continuar\nDigite [N] para encerrar')
-    while True:#while infinito n2
-        continuar = str(input('Sua opção:')).strip().upper()
-        if continuar == 'S' or continuar == 'N':
-            break#break n2
+    continuar = ' '
+    while continuar != 'S' and continuar != 'N':  # while continuar diferente de 's' e 'n' faça
+        continuar = str(input('Sua opção:')).strip().upper()[0]
     if continuar == 'N':
-        break#break n1
+        break  # break n1
     contador += 1
 print('-'*40)
 print(f'Os produtos selecionados foram {produtos}')
